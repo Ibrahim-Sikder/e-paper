de21877065c94916c6dc82e066ac90f820e1fff2
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2026-04-06 14:44:32 +0600

    pagination-done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-"use client";
+import NewspaperViewer from "@/components/ui/home/NewspaperViewer";
 
-import NewsPaper from "@/components/ui/home/e-paper/NewsPaper";
-import NewsTopBar from "@/components/ui/home/NewsTopBar";
-export default function NewspaperViewer() {
-  return (
-    <div>
-      <NewsTopBar />
-      <NewsPaper />
-    </div>
-  );
+export default function Page() {
+  return <NewspaperViewer />;
 }
